b65530c5b5294df0e269148eb45bac924e51411b
Alvaro Perez Pintado <122454645+AlvaroNW@users.noreply.github.com>
2023-02-19 01:04:12 +0100

    fixed status method and formating

--- pet.js
+++ pet.js
@@ -1,7 +1,15 @@
 // class constructor
 
 class Pet {
-    constructor(name, tiredness = 5, hunger = 5, boredness = 5, education = 0, health = 30, stress = 0) {
+    constructor(
+        name,
+        tiredness = 5,
+        hunger = 5,
+        boredness = 5,
+        education = 0,
+        health = 30,
+        stress = 0
+    ) {
         this.name = name;
         this.tiredness = tiredness;
         this.hunger = hunger;
@@ -10,118 +18,143 @@ class Pet {
         this.health = health;
         this.stress = stress;
     }
-    sleep(){
+    sleep() {
         this.tiredness--,
-        displayedText.textContent = 'Tiredness reduced by 1';
-        
+            (displayedText.textContent = "Tiredness reduced by 1");
     }
-    feed(){
+    feed() {
         this.hunger--;
-        displayedText.textContent = 'Hunger reduced by 1';
+        displayedText.textContent = "Hunger reduced by 1";
     }
-    play(){
+    play() {
         this.boredness--,
-        displayedText.textContent = 'Boredness reduced by 1';
+            (displayedText.textContent = "Boredness reduced by 1");
     }
-    educate(){
-        this.education++,
-        this.boredness++,
-        this.hunger++,
-        this.tiredness++;
+    educate() {
+        this.education++, this.boredness++, this.hunger++, this.tiredness++;
         displayedText.textContent = `Education increased by 1! ${this.name} is more tired, bored and hungry than before!`;
     }
-    healthCheck(){
-        const distress = (this.health - this.stress)
+    healthCheck() {
+        const distress = this.health - this.stress;
         healthDisplay.textContent = `+${distress}`;
         if (this.health <= this.stress) {
-            screen.id='gameover';
-            displayedText.textContent = 'GAME OVER';
-            healthDisplay.textContent ='';
+            screen.id = "gameover";
+            displayedText.textContent = "GAME OVER";
+            healthDisplay.textContent = "";
         }
-        
     }
-    stressCalc(){
-        this.stress = (this.tiredness + this.hunger + this.boredness); //needed separate from health
+    stressCalc() {
+        this.stress = this.tiredness + this.hunger + this.boredness; //needed separate from health
     }
-    status(){
-        console.log(`${this.name}'s tiredness is ${this.tiredness} , hunger is ${this.hunger} lonelines is ${this.boredness}`)
-        if (this.tiredness >= 5 && this.hunger >= 5  && this.boredness >= 5) {
-            displayedText.textContent =`${this.name} is really tired, really hungry and really lonely!!`;
-
-        }else if(this.tiredness > 3 && this.hunger > 3 && this.boredness > 3){
-            displayedText.textContent =`${this.name} is somewhat tired, somewhat hungry and somewhat lonely`;
-
-        }else if(this.tiredness < 3 && this.hunger < 3 && this.boredness < 3){
-            displayedText.textContent =`${this.name} is well taken care of!!`;
-
-        }else if(this.tiredness > 3 && this.hunger < 3 && this.boredness < 3){
-            displayedText.textContent =`${this.name} is tired!!`;
-        }else if(this.tiredness < 3 && this.hunger > 3 && this.boredness < 3){
-            displayedText.textContent =`${this.name} is hungry!!`;
-        }else if(this.tiredness < 3 && this.hunger < 3 && this.boredness > 3){
-            displayedText.textContent =`${this.name} is lonely!`;
+    status() {
+        console.log(
+            `${this.name}'s tiredness is ${this.tiredness} , hunger is ${this.hunger} lonelines is ${this.boredness}`
+        );
+        if (this.tiredness >= 5 && this.hunger >= 5 && this.boredness >= 5) {
+            displayedText.textContent = `${this.name} is really tired, really hungry and really lonely!!`;
+        } else if (
+            this.tiredness > 3 &&
+            this.hunger > 3 &&
+            this.boredness > 3
+        ) {
+            displayedText.textContent = `${this.name} is somewhat tired, somewhat hungry and somewhat lonely`;
+        } else if (
+            this.tiredness <= 3 &&
+            this.hunger <= 3 &&
+            this.boredness <= 3
+        ) {
+            displayedText.textContent = `${this.name} is well taken care of!!`;
+        } else if (
+            this.tiredness > 3 &&
+            this.hunger > 3 &&
+            this.boredness <= 3
+        ) {
+            displayedText.textContent = `${this.name} is tired & hungry!!`;
+        } else if (
+            this.tiredness <= 3 &&
+            this.hunger > 3 &&
+            this.boredness > 3
+        ) {
+            displayedText.textContent = `${this.name} is bored & hungry!!`;
+        } else if (
+            this.tiredness > 3 &&
+            this.hunger <= 3 &&
+            this.boredness > 3
+        ) {
+            displayedText.textContent = `${this.name} is tired & bored!!`;
+        } else if (
+            this.tiredness > 3 &&
+            this.hunger <= 3 &&
+            this.boredness <= 3
+        ) {
+            displayedText.textContent = `${this.name} is tired!!`;
+        } else if (
+            this.tiredness <= 3 &&
+            this.hunger > 3 &&
+            this.boredness <= 3
+        ) {
+            displayedText.textContent = `${this.name} is hungry!!`;
+        } else if (
+            this.tiredness <= 3 &&
+            this.hunger <= 3 &&
+            this.boredness >= 3
+        ) {
+            displayedText.textContent = `${this.name} is lonely!`;
         }
-        
     }
-};
-
-
+}
 
 // -----------------------DOM---------------
 
 //selectors
-const feedBtn = document.querySelector('.button__left');
-const sleepBtn = document.querySelector('.button__center');
-const playBtn = document.querySelector('.button__right');
-const statusBtn = document.querySelector('.button__leftside');
-const educateBtn = document.querySelector('.button__rightside');
-const screen = document.querySelector('.healthbar__container');
-const displayedText = document.querySelector('#displayedText');
-const healthMeter = document.querySelector('#health-meter-hidden');
-const healthDisplay = document.querySelector('.health');
-
+const feedBtn = document.querySelector(".button__left");
+const sleepBtn = document.querySelector(".button__center");
+const playBtn = document.querySelector(".button__right");
+const statusBtn = document.querySelector(".button__leftside");
+const educateBtn = document.querySelector(".button__rightside");
+const screen = document.querySelector(".healthbar__container");
+const displayedText = document.querySelector("#displayedText");
+const healthMeter = document.querySelector("#health-meter-hidden");
+const healthDisplay = document.querySelector(".health");
 
 //Play listeners
-feedBtn.addEventListener('click', e =>{
+feedBtn.addEventListener("click", (e) => {
     tamagotchi.feed();
-    tamagotchi.stressCalc() //everytime calc stress and health after every action
+    tamagotchi.stressCalc(); //everytime calc stress and health after every action
     tamagotchi.healthCheck();
-
-})
-sleepBtn.addEventListener('click', e =>{
+});
+sleepBtn.addEventListener("click", (e) => {
     tamagotchi.sleep();
-    tamagotchi.stressCalc()
+    tamagotchi.stressCalc();
     tamagotchi.healthCheck();
-})
-playBtn.addEventListener('click', e =>{
+});
+playBtn.addEventListener("click", (e) => {
     tamagotchi.play();
-    tamagotchi.stressCalc()
+    tamagotchi.stressCalc();
     tamagotchi.healthCheck();
-})
-statusBtn.addEventListener('click', e =>{
+});
+statusBtn.addEventListener("click", (e) => {
     tamagotchi.status();
-    tamagotchi.stressCalc()
+    tamagotchi.stressCalc();
     tamagotchi.healthCheck();
-    
-})
-educateBtn.addEventListener('click', e =>{
+});
+educateBtn.addEventListener("click", (e) => {
     tamagotchi.educate();
-    tamagotchi.stressCalc()
+    tamagotchi.stressCalc();
     tamagotchi.healthCheck();
-})
+});
 
 // creating pet
-let tamagotchi
-const inputEntry = document.querySelector('#input');
+let tamagotchi;
+const inputEntry = document.querySelector("#input");
 
-inputEntry.addEventListener('keydown', (e) =>{
-    // 
-    if(e.key === 'Enter'){
+inputEntry.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
         tamagotchi = new Pet(inputEntry.value);
-        const formObj = document.querySelector('#form');
+        const formObj = document.querySelector("#form");
         formObj.removeChild(inputEntry);
-        healthMeter.id='health-meter';
-        tamagotchi.stressCalc(); //starting stress calc 
+        healthMeter.id = "health-meter";
+        tamagotchi.stressCalc(); //starting stress calc
         tamagotchi.healthCheck(); //displaying health
     }
-})
+});
